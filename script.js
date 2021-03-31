6f26dfdbb15ccc407ffc409d7640da493b36dda7
mckale <71574223+mckalechung@users.noreply.github.com>
2021-03-30 21:29:48 -0600

    Full Site!

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    window.addEventListener('scroll', myFunction);
-    window.onscroll = function() {myFunction()};
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-    
-    
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
-      }
+  window.addEventListener('scroll', myFunction);
+  window.onscroll = function() {myFunction()};
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+  
+  
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky")
+    } else {
+      navbar.classList.remove("sticky");
     }
-    })
+  }
+  })
+  
+  
+  
